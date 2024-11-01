cc5f6d15c174fa1e679700f838deb528535fcf58
Seth-Hoppenstein <shoppenstein@smu.edu>
2024-10-31 23:41:20 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
+#include "SentimentClassifier.h"
 #include <iostream>
 
+int main(int argc, char* argv[]) {
+    if (argc != 5) {
+        std::cerr << "Usage: " << argv[0] << " training_file testing_file ground_truth_file results_file accuracy_file" << std::endl;
+        return 1;
+    }
 
-int main(int argc, char** argv)
-{  
-    std::cout << "I will be a sentiment analyzer!" << std::endl;
+    SentimentClassifier classifier;
+    classifier.train(DSString(argv[1]));
+    classifier.evaluate(DSString(argv[2]), DSString(argv[3]), DSString(argv[4]), DSString(argv[5]));
 
     return 0;
 }
