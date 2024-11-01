b0c42a631081c3981cb3d3798c3e69d1560c14ef
Seth-Hoppenstein <shoppenstein@smu.edu>
2024-10-31 23:35:53 -0500

    Update DSString.cpp

--- DSString.cpp
+++ DSString.cpp
@@ -1,10 +1,54 @@
 #include "DSString.h"
+#include <cstring>
 
-/* 
- * Implement the functions defined in DSString.h. You may add more functions as needed
- * for the project. 
- *
- * Note that c-strings don's store an explicit length but use `\0` as the terminator symbol
- * but your class should store its length in a member variable. 
- * DDO NOT USE C-STRING FUNCTIONS <string.h> or <cstring>.
- */  
+DSString::DSString() : data(nullptr), length(0) {}
+
+DSString::DSString(const char* str) {
+    length = strlen(str);
+    data = new char[length + 1];
+    strcpy(data, str);
+}
+
+DSString::DSString(const DSString& other) {
+    length = other.length;
+    data = new char[length + 1];
+    strcpy(data, other.data);
+}
+
+DSString::~DSString() {
+    delete[] data;
+}
+
+DSString& DSString::operator=(const DSString& other) {
+    if (this != &other) {
+        delete[] data;
+        length = other.length;
+        data = new char[length + 1];
+        strcpy(data, other.data);
+    }
+    return *this;
+}
+
+size_t DSString::getLength() const {
+    return length;
+}
+
+const char* DSString::c_str() const {
+    return data;
+}
+
+void DSString::append(const char* str) {
+    size_t newLength = length + strlen(str);
+    char* newData = new char[newLength + 1];
+    strcpy(newData, data);
+    strcat(newData, str);
+    delete[] data;
+    data = newData;
+    length = newLength;
+}
+
+void DSString::clear() {
+    delete[] data;
+    data = nullptr;
+    length = 0;
+}
